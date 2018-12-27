e78dfbd346974fa34f9a9df9c743e0b34fbe4f56
tskim <lollolzkk@gmail.com>
2018-12-27 22:31:04 +0900

    분수찾기(1193) 풀이

--- baekjoon/분수찾기(1193).cpp
+++ baekjoon/분수찾기(1193).cpp
@@ -2,7 +2,7 @@
 #define MAX 10000000
 using namespace std;
 int input, temp;
-int up, down;		//분자,분모
+int up, down;	//분자,분모
 
 int main() {
 	cin >> input;
@@ -11,7 +11,7 @@ int main() {
 		if (temp <= 0) {
 			//짝수
 			if (i % 2 == 0) {
-				up = input;				//temp에 의해 가공된 input은 찾고자 하는 숫자가 포함된 라인에서, 몇 번째에 위치하는지를 의미
+				up = input;	//temp에 의해 가공된 input은 찾고자 하는 숫자가 포함된 라인에서, 몇 번째에 위치하는지를 의미
 				down = i + 1 - up;	//i는 몇 번째 라인인지를 의미
 				break;
 			}
