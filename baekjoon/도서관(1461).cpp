fcca9bdf660dd810929cd7585bc3c954265fead1
tskim <lollolzkk@gmail.com>
2018-12-29 17:07:13 +0900

    백준 도서관(1461) 풀이

---
+++ baekjoon/도서관(1461).cpp
@@ -0,0 +1,106 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <stdlib.h>
+//
+//using namespace std;
+//int N, M;
+//int ans;
+//vector <int> v;
+//
+//void input() {
+//	cin >> N >> M;
+//	int x;
+//	for (int i = 0; i < N; i++) {
+//		cin >> x;
+//		v.push_back(x);
+//	}
+//}
+//
+//void findAns() {
+//	sort(v.begin(), v.end());
+//	if (abs(v.front()) >= abs(v.back())) {
+//		ans += abs(v.front());
+//		for (int i = M; v[i] < 0 && i < v.size(); i += M) {
+//			ans += abs(v[i])*2;
+//		}
+//		for (int i = v.size() - 1; v[i] > 0 && i >= 0; i -= M) {
+//			ans += v[i]*2;
+//		}
+//	}
+//	else {
+//		ans += abs(v.back());
+//		for (int i = 0; v[i] < 0 && i < v.size(); i += M) {
+//			ans += abs(v[i]) * 2;
+//		}
+//		for (int i = (v.size() - 1) - M; v[i] > 0 && i >= 0; i -= M) {
+//			ans += v[i] * 2;
+//		}
+//	}
+//}
+//
+//int main() {
+//	input();
+//	findAns();
+//	cout << ans;
+//	return 0;
+//}
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <queue>
+#include <stack>
+
+using namespace std;
+int N, M;
+int ans, maxVal;
+vector <int> v;
+queue <int> q;
+stack <int> s;
+
+void input() {
+	cin >> N >> M;
+	int x;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
+}
+
+void findAns() {
+	sort(v.begin(), v.end());
+	maxVal = abs(v.front()) > abs(v.back()) ? abs(v.front()) : abs(v.back());
+
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] < 0) {
+			q.push(abs(v[i]));
+		}
+		if (v[i] > 0) {
+			s.push(v[i]);
+		}
+	}
+	while (!q.empty()) {
+		ans += q.front() * 2;
+		for (int i = 0; i < M; i++) {
+			q.pop();
+			if (q.empty()) break;
+		}
+	}
+	while (!s.empty()) {
+		ans += s.top() * 2;
+		for (int i = 0; i < M; i++) {
+			s.pop();
+			if (s.empty()) break;
+		}
+	}
+}
+
+int main() {
+	input();
+	findAns();
+	cout << ans-maxVal;
+	return 0;
+}
