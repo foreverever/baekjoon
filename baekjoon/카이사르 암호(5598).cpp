4d44888cd7cc79a7a6c2e9bcf3817352f0937e48
tskim <lollolzkk@gmail.com>
2019-01-11 19:12:12 +0900

    카이사르 암호(5598) 풀이

---
+++ baekjoon/카이사르 암호(5598).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+int main() {
+	cin >> str;
+	for (char s : str) {
+		if (s < 'D') {
+			cout << char('Z' - ('C'-s));
+		}
+		else {
+			cout << char(s - 3);
+		}
+	}
+	return 0;
+}
