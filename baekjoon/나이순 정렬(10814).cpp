5e073449da8ff428a18e6f53385af0d35b685a9a
TaeSu <lollolzkk@gmail.com>
2019-03-14 21:44:57 +0900

    백준 나이순 정렬(10814) 풀이 기존 stable_sort 안쓰고 다른 풀이

--- baekjoon/나이순 정렬(10814).cpp
+++ baekjoon/나이순 정렬(10814).cpp
@@ -1,33 +1,68 @@
+////#include <iostream>
+////#include <utility>
+////#include <algorithm>
+////#include <string>
+////#include <vector>
+////using namespace std;
+////
+////int n;
+////int age;
+////string str;
+////vector <pair<int, string> > v;
+////pair<int, string> p;
+////
+////bool compare(const pair<int, string>&i, const pair<int, string>&j)
+////{
+////	if (i.first == j.first)
+////		return 0;
+////	return i.first < j.first;
+////}
+////
+////int main() {
+////	cin >> n;
+////	for (int i = 1; i <= n; i++) {
+////		cin >> age >> str;
+////		v.push_back(make_pair(age, str));
+////	}
+////	stable_sort(v.begin(), v.end(), compare);
+////
+////	for (int i = 0; i < n; i++) {
+////		cout << v[i].first << ' ' << v[i].second << '\n';
+////	}
+////	return 0;
+////}
+//
+//
+/////////////////////////////////////////// 84ms -> ios_base::sync_with_stdio(false), cin.tie(0) : 48ms
 #include <iostream>
-#include <utility>
-#include <algorithm>
 #include <string>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
 int n;
-int age;
-string str;
-vector <pair<int, string> > v;
-pair<int, string> p;
+struct Person {
+	int age, idx;
+	string name;
+};
 
-bool compare(const pair<int, string>&i, const pair<int, string>&j)
-{
-	if (i.first == j.first)
-		return 0;
-	return i.first < j.first;
+Person p[100001];
+
+bool cmp(Person &p1, Person &p2) {
+	if (p1.age == p2.age) return p1.idx < p2.idx;
+	return p1.age < p2.age;
 }
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> age >> str;
-		v.push_back(make_pair(age, str));
-	}
-	stable_sort(v.begin(), v.end(), compare);
-
 	for (int i = 0; i < n; i++) {
-		cout << v[i].first << ' ' << v[i].second << '\n';
+		cin >> p[i].age >> p[i].name;
+		p[i].idx = i;
+	}
+	sort(p, p + n, cmp);
+	for (int i = 0; i < n; i++) {
+		cout << p[i].age << ' ' << p[i].name << '\n';
 	}
 	return 0;
 }
