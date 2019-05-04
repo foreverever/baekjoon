476dd258dc50bc1211ff9cd32234eb8846cbbfee
TaeSu <lollolzkk@gmail.com>
2019-05-04 14:22:20 +0900

    프로그래머스_다음 큰 숫자(lev2) 풀이

--- baekjoon/프로그래머스_다음 큰 숫자.cpp
+++ baekjoon/프로그래머스_다음 큰 숫자.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int getCount(int n) {
 	vector<int> binary;
+
 	while (n) {
 		binary.push_back(n % 2);
 		n /= 2;
 	}
 
 	int cnt = 0;
+
 	for (int v : binary) {
 		if (v) cnt++;
 	}
@@ -28,3 +30,5 @@ int solution(int n) {
 	}
 	return answer;
 }
+//입력받은 숫자 n부터 1씩 증가한 숫자의 이진수를 구한 후, 
+//해당 이진수의 1의 개수가 n의 이진수의 1의 개수와 같은 최초의 숫자가 n 다음 큰 숫자가 된다.
